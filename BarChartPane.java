8ddc9ee371d9b87f53502bd4ce10c04da2e7fe05
Mohamad Ahmed <mohamad.ahmed@kcl.ac.uk>
2016-11-24 19:03:31 +0000

    Adds inline comments

--- BarChartPane.java
+++ BarChartPane.java
@@ -4,14 +4,17 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.layout.Pane;
 
 public class BarChartPane extends Pane {
+    //Bar Chart object of the Pane
     private BarChart<String, Number> barChart;
 
     public BarChartPane() {
         super();
 
+        //Creates x and y axis
         CategoryAxis dateAxis = new CategoryAxis();
         NumberAxis valueAxis = new NumberAxis();
 
+        //Initialises empty bar chart
         barChart = new BarChart<>(dateAxis, valueAxis);
     }
 }
