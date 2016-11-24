8da1c0b4ea79b95c0c6e8f203c09af6ba48c777d
Khalil Ahmed <mkhalila@outlook.com>
2016-11-24 20:42:34 +0000

    Bar Chart resizes with size of container

--- BarChartPane.java
+++ BarChartPane.java
@@ -2,9 +2,9 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
-public class BarChartPane extends Pane {
+public class BarChartPane extends StackPane {
     //Bar Chart object of the Pane
     private BarChart<String, Number> barChart;
 
