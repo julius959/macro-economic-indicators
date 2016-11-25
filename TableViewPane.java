7d68ce297c26d4bff384165c71e790234b1a5716
Khalil Ahmed <mkhalila@outlook.com>
2016-11-25 01:29:18 +0000

    Initialises sample table columns and adds to table

--- TableViewPane.java
+++ TableViewPane.java
@@ -1,10 +1,11 @@
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 
 public class TableViewPane extends StackPane {
-    private TableView table;
+    private TableView<String> table;
 
     public TableViewPane() {
         super();
@@ -14,8 +15,13 @@ public class TableViewPane extends StackPane {
         tableTitle.setFont(new Font("Arial", 20));
 
         //Initialises uneditable table
-        table = new TableView();
+        table = new TableView<>();
         table.setEditable(false);
+
+        //Initialises table columns and adds them to the table
+        TableColumn<String, String> tbcolDate = new TableColumn<>("X");
+        TableColumn<String, Number> tbcolValue = new TableColumn<>("Y");
+        table.getColumns().addAll(tbcolDate, tbcolValue);
 
     }
 }
