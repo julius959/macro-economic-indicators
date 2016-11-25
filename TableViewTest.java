1e64e1e9741317bb6f27312816a214dda1b373ce
Khalil Ahmed <mkhalila@outlook.com>
2016-11-25 01:33:36 +0000

    Creates test class for TableView - creates and displays empty table

---
+++ TableViewTest.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TableViewTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Table View Test");
+
+        Scene primaryScene = new Scene(new TableViewPane());
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
