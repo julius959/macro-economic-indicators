4763392711320f1181269586c833cf871fa7ccd3
Mohamad Ahmed <mohamad.ahmed@kcl.ac.uk>
2016-11-26 21:43:20 +0000

    NewsFeedPane retrieves news from NewsFeed

--- NewsFeedPane.java
+++ NewsFeedPane.java
@@ -3,6 +3,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import java.util.HashMap;
+
 public class NewsFeedPane extends BorderPane {
 
     public NewsFeedPane() {
@@ -12,5 +14,15 @@ public class NewsFeedPane extends BorderPane {
         Label lblTitle = new Label("Latest news from the BBC");
         HBox hbTitle = new HBox(lblTitle);
         hbTitle.setAlignment(Pos.CENTER);
+        setTop(hbTitle);
+
+        //Retrieve news
+        try {
+            HashMap<String, String> hmNews = NewsFeed.getNews();
+        } catch(Exception e) {
+            setCenter(new Label("Failed to retrieve News. " +
+                    "Please ensure you have an active Internet connection"));
+        }
+
     }
 }
