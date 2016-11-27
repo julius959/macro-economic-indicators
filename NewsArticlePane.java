efed134e8e578c49ae0ebe4f3b9d81d6adcf6db4
Khalil Ahmed <mkhalila@outlook.com>
2016-11-27 18:07:21 +0000

    News feed entries now include article image

--- NewsArticlePane.java
+++ NewsArticlePane.java
@@ -2,18 +2,18 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.util.Date;
-
 public class NewsArticlePane extends BorderPane {
 
-    public NewsArticlePane(String titleIn, String linkIn, String descriptionIn, String publishDateIn) {
+    public NewsArticlePane(String titleIn, String linkIn, String descriptionIn, String publishDateIn, String imgURLIn) {
         super();
 
         //Temporary styling
@@ -34,6 +34,22 @@ public class NewsArticlePane extends BorderPane {
         setTop(hbTitle);
         setCenter(lblDescription);
         setBottom(hbDate);
+
+        //Retrieves news articles image from url
+        ImageView imageView = ImageViewBuilder.create()
+                .image(new Image(imgURLIn))
+                .build();
+
+        //Resizes the image to have width of 100 while preserving the ratio and using
+        // higher quality filtering method; this ImageView is also cached to
+        // improve performance
+        imageView.setFitWidth(100);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+
+        //Adds the articles image to the news entry
+        setLeft(imageView);
 
         //Action Listener for clicking on an article
         //Opens up article in web view
