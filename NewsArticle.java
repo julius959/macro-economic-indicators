bdcf28af3be37a62d64939c98f8fdc3f561c3fa7
Khalil Ahmed <mkhalila@outlook.com>
2016-11-27 16:05:49 +0000

    Implements NewsArticle class

---
+++ NewsArticle.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+
+public class NewsArticle {
+    private String title, link, description;
+    private Date publishDate;
+
+    public NewsArticle(String titleIn, String linkIn, String descriptionIn, Date publishDateIn) {
+        title = titleIn;
+        link = linkIn;
+        description = descriptionIn;
+        publishDate = publishDateIn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+}
