22622376484e3fef55695f9e43640d98018cbf51
jacobklerfelt <jacob.klerfelt@kcl.ac.uk>
2016-11-28 01:49:36 +0000

    Extending implementation of linecharts by adding y-value of line whenever a user hovers over it. Will style more when talking to the others so the programs style layout is consistent throughout program

--- Charts/src/LineCharts.java
+++ Charts/src/LineCharts.java
@@ -1,6 +1,9 @@
+import javafx.event.EventHandler;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +23,28 @@ public class LineCharts extends StackPane {
         lineChart.setTitle("GDP");
         this.passData(data);
         this.getChildren().add(lineChart);
+        for(XYChart.Series<String, Number> series : lineChart.getData()) {
+            for (final XYChart.Data<String, Number> node : series.getData()) {
+                node.getNode().setOnMouseEntered(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        Label caption = new Label("");
+                        caption.setStyle("-fx-font: 35 arial;");
+                        caption.setStyle("-fx-background-color: #FFFFFF");
+                        caption.setTranslateX(event.getSceneX());
+                        caption.setTranslateY(event.getSceneY());
+                        caption.setText(String.valueOf(node.getYValue()));
+                        getChildren().add(caption);
+                    }
+                });
+                node.getNode().setOnMouseExited(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        getChildren().remove(getChildren().size() - 1);
+                    }
+                });
+            }
+        }
     }
 
     private void passData(ArrayList <HashMap<String,Integer>> data){
