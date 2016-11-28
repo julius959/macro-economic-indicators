3789ec74be2ffbf94f4a9f7586e5cfb6a1ef1e51
jacobklerfelt <jacob.klerfelt@kcl.ac.uk>
2016-11-28 01:16:24 +0000

    Extending piecharts by adding label for each pie with the value as well adding a eventhandler that displays the value for a slice of the pie whenever the user hovers over a slice

--- Charts/src/PieCharts.java
+++ Charts/src/PieCharts.java
@@ -1,5 +1,8 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.chart.*;
 import java.util.ArrayList;
@@ -10,30 +13,53 @@ import java.util.HashMap;
  */
 public class PieCharts extends StackPane {
 
-
     private PieChart pieChart;
+    private ObservableList<javafx.scene.chart.PieChart.Data> pieChartData;
 
     public PieCharts(ArrayList<HashMap<String, Integer>> data){
 
         super();
         pieChart = new PieChart();
-        pieChart.setTitle("GDP");
-        this.passData(data);
+        pieChart.setTitle(""); // waiting for info from Vlad about where to get this info - wrapper.
+        this.addData(data);
         this.getChildren().add(pieChart);
+        pieChart.setLabelsVisible(true);
+        for (final PieChart.Data dataInPie : pieChart.getData()) {
+            dataInPie.getNode().setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent event) {
+                    Label caption = new Label("");
+                    caption.setStyle("-fx-font: 35 arial;");
+                    caption.setStyle("-fx-background-color: #FFFFFF");
+                    caption.setTranslateX(event.getSceneX());
+                    caption.setTranslateY(event.getSceneY());
+                    caption.setText(String.valueOf(dataInPie.getName()) + " - " +  dataInPie.getPieValue() + "%");
+                    getChildren().add(caption);
+
+                }
+            });
+            dataInPie.getNode().setOnMouseExited(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    getChildren().remove(getChildren().size()-1);
+                }
+            });
+        }
     }
 
-    private void passData(ArrayList<HashMap<String, Integer>> data){
+    private void addData(ArrayList<HashMap<String, Integer>> data){
 
-        ObservableList<javafx.scene.chart.PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        pieChartData = FXCollections.observableArrayList();
 
         for(HashMap<String, Integer> temp : data){
             int average = 0;
             for(int value : temp.values()){
                 average += value;
             }
-            pieChartData.add(new javafx.scene.chart.PieChart.Data("Country", average/temp.size()));
+            // get country from wrapper provided by Vlad - leaving for now
+            pieChartData.add(new PieChart.Data("Country", average/temp.size()));
         }
         pieChart.getData().addAll(pieChartData);
     }
-
 }
