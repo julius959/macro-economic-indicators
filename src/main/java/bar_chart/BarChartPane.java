b95afb8b1fe716e8f15c3e016cac99cd06ba1cb0
Mohamad Ahmed <mohamad.ahmed@kcl.ac.uk>
2016-12-11 23:52:36 +0000

    Adds inline comments and JavaDoc

--- src/main/java/bar_chart/BarChartPane.java
+++ src/main/java/bar_chart/BarChartPane.java
@@ -1,16 +1,12 @@
 package bar_chart;
 
 import api_model.Model;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
-import javafx.geometry.Bounds;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.StackPane;
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.TreeMap;
@@ -19,13 +15,16 @@ public class BarChartPane extends StackPane {
     //Bar Chart object of the Pane
     private BarChart<String, Number> barChart;
 
+    /**
+     * Constructor for a BarChartPane
+     * @param data ArrayList containing a TreeMap for every country that has data to be displayed
+     */
     public BarChartPane(ArrayList<TreeMap<Integer, Number>> data) {
         super();
 
         //Creates x and y axis
         CategoryAxis dateAxis = new CategoryAxis();
         NumberAxis valueAxis = new NumberAxis();
-
 
         //Initialises empty bar chart
         barChart = new BarChart<>(dateAxis, valueAxis);
@@ -38,30 +37,34 @@ public class BarChartPane extends StackPane {
 
         //Populate chart with given data
         passData(data);
+
         //Add chart to container
-
         getChildren().add(barChart);
-
-
     }
 
-
+    /**
+     * Updates bar chart with given data
+     * @param data ArrayList containing a TreeMap for every country that has data to be displayed
+     */
     public void passData(ArrayList<TreeMap<Integer, Number>> data) {
+        //Clears current data in bar chart - when year's requested has changed
         barChart.getData().clear();
 
+        //Retrieves name of current indicator
         String chartTitle = Model.getInstance().currentObjectIndicator.getLabelFromCode(Model.getInstance().currentIndicator);
 
+        //Store data to 3 decimal places
         DecimalFormat yValFormat = new DecimalFormat(".###");
+
         //Loop over data for every country
         for (int i = 0; i < data.size(); ++i) {
             //Create new series of data for each country
             XYChart.Series<String, Number> tempSeries = new XYChart.Series<>();
-            //Add key to graph for country
+            //Retrieve corresponding country for this series of data and add it to name of this series
             String country = Model.getInstance().countries[Model.getInstance().currentCountries.get(i)].getName();
             tempSeries.setName(country);
             //Get the date values
             ArrayList<Integer> alstKeys = new ArrayList<>(data.get(i).keySet());
-            System.out.println("values " + tempSeries);
             //Add each data instance to the series of data
             for (Integer sKey : alstKeys)
                 tempSeries.getData().add(new XYChart.Data<>(Integer.toString(sKey), data.get(i).get(sKey)));
@@ -70,6 +73,7 @@ public class BarChartPane extends StackPane {
             barChart.getData().add(tempSeries);
             barChart.setAnimated(false);
 
+            //Add a on-mouse-hover tooltip displaying the (x,y) value of the node and country it represents
             for (final XYChart.Data<String, Number> node : tempSeries.getData()) {
                 Tooltip.install(node.getNode(),
                         new Tooltip("Country: " + country + "\nDate: " + node.getXValue() +
@@ -77,7 +81,5 @@ public class BarChartPane extends StackPane {
             }
 
         }
-        System.out.println(barChart.getData());
-
     }
 }
